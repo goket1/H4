890d96d92a6a61af2a506a4f5012afdb423781d1
goket1 <goket@protonmail.com>
2020-10-27 14:59:35 +0100

    Login system part 1 done

---
+++ Crypto/Crypto/LoginSystem/Hash.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Security.Cryptography;
+
+namespace LoginSystem
+{
+    public class Hash
+    {
+        private SHA512 sha512 = new System.Security.Cryptography.SHA512CryptoServiceProvider();
+        
+        public bool CheckAuthenticity(byte[] mes, byte[] mac)
+        {
+            return (CompareByteArrays(mes, mac, mes.Length));
+        }
+        public bool CompareByteArrays(byte[] a, byte[] b, int len)
+        {
+            for (int i = 0; i < len; i++)
+                if (a[i] != b[i])
+                    return false;
+            return true;
+        }
+        
+        public byte[] ComputePasswordHash(byte[] pass, byte[] salt)
+        {
+            var combined = Combine(pass, salt);
+            var hashed = sha512.ComputeHash(combined);
+            // Console.WriteLine($"ComputePasswordHash called with: \nPassword: len: {pass.Length} |{BitConverter.ToString(pass)}|\nSalt: len: {salt.Length} |{BitConverter.ToString(salt)}|\nCombined: len {combined.Length} |{BitConverter.ToString(combined)}|\nHashed: len {hashed.Length} |{BitConverter.ToString(hashed)}|");
+            return hashed;
+        }
+        
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+
+            return ret;
+        }
+        
+        public byte[] GenerateSalt(int saltLength = 16)
+        {
+            using (RNGCryptoServiceProvider cryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                byte[] random = new byte[saltLength];
+                cryptoServiceProvider.GetBytes(random);
+                return random;
+            }
+        }
+    }
+}
--- Crypto/Crypto/LoginSystem/Program.cs
+++ Crypto/Crypto/LoginSystem/Program.cs
@@ -1,13 +1,117 @@
 ﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Text;
 using Npgsql;
 
 namespace LoginSystem
 {
     class Program
     {
+        static int Menu(string message, int optionCount)
+        {
+            bool invalid = true;
+            int selected = 0;
+
+            while (invalid)
+            {
+                Console.Write($"{message}\n: ");
+                string input = Console.ReadLine();
+                invalid = !(int.TryParse(input, out selected) & selected > 0 & selected <= optionCount);
+            }
+
+            return selected;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hash hash = new Hash();
+            var cs = "Host=localhost;Username=postgres;Password=s$cret;Database=h4loginsystem";
+            using (var con = new NpgsqlConnection(cs))
+            {
+                con.Open();
+
+                using (var cmd = new NpgsqlCommand("SELECT version()", con))
+                {
+                    var version = cmd.ExecuteScalar();
+                    Console.WriteLine($"Connected to postgreSQL version: {version}");
+                }
+
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                switch (Menu("Select\n1. Create user\n2. Login", 2))
+                {
+                    case 1:
+                        byte[] salt = hash.GenerateSalt();
+                        byte[] hashed = hash.ComputePasswordHash(Encoding.UTF8.GetBytes(password), salt);
+                        // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(salt)}\nPassword hash: {BitConverter.ToString(hashed)}\nLen: {hashed.Length}");
+
+                        using (var cmd = new NpgsqlCommand("CALL create_user(@username, @password, @salt)", con))
+                        {
+                            cmd.Parameters.AddWithValue("username", username);
+                            cmd.Parameters.AddWithValue("password", hashed);
+                            cmd.Parameters.AddWithValue("salt", salt);
+                            ;
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        break;
+                    case 2:
+                        byte[] db_password = new byte[64];
+                        byte[] db_salt = new byte[16];
+                        int db_attempts = 0;
+
+                        using (var cmd = new NpgsqlCommand("get_user", con))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("in_username", username);
+                            using (var reader = cmd.ExecuteReader())
+                            {
+                                Console.WriteLine();
+                                while (reader.Read())
+                                {
+                                    reader.GetString(0);
+                                    reader.GetBytes(1, 0, db_password, 0, 64);
+                                    reader.GetBytes(2, 0, db_salt, 0, 16);
+                                    db_attempts = reader.GetInt16(3);
+                                }
+                            }
+                        }
+
+                        if (db_attempts > 5)
+                        {
+                            Console.WriteLine("User account locked due to too many login attempts");
+                        }
+                        else
+                        {
+                            
+                            byte[] hashFromUser = hash.ComputePasswordHash(Encoding.UTF8.GetBytes(password), db_salt);
+                            // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(db_salt)}\nPassword hash from user: {BitConverter.ToString(hashFromUser)}\nPassword hash in DB: {BitConverter.ToString(db_password)}\nLen: {hashFromUser.Length}");
+                            if (hash.CheckAuthenticity(hashFromUser, db_password))
+                            {
+                                Console.WriteLine("Password correct!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Password incorrect :(");
+                                using (var cmd = new NpgsqlCommand("CALL wrong_password(@username, @attempts)", con))
+                                {
+                                    cmd.Parameters.AddWithValue("username", username);
+                                    cmd.Parameters.AddWithValue("attempts", db_attempts + 1);
+                                    ;
+                                    cmd.ExecuteNonQuery();
+                                }
+                            }
+
+                        }
+
+                        break;
+                }
+            }
         }
     }
 }
