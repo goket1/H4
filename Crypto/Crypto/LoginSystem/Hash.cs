20d46478592c8c8c7773f07532d751e6b5374a1c
goket1 <goket@protonmail.com>
2020-10-28 07:47:11 +0100

    Updated login system to use Password Based Key Derivation

--- Crypto/Crypto/LoginSystem/Hash.cs
+++ Crypto/Crypto/LoginSystem/Hash.cs
@@ -19,12 +19,21 @@ namespace LoginSystem
             return true;
         }
         
-        public byte[] ComputePasswordHash(byte[] pass, byte[] salt)
+        /*public byte[] ComputePasswordHash(byte[] pass, byte[] salt)
         {
             var combined = Combine(pass, salt);
             var hashed = sha512.ComputeHash(combined);
             // Console.WriteLine($"ComputePasswordHash called with: \nPassword: len: {pass.Length} |{BitConverter.ToString(pass)}|\nSalt: len: {salt.Length} |{BitConverter.ToString(salt)}|\nCombined: len {combined.Length} |{BitConverter.ToString(combined)}|\nHashed: len {hashed.Length} |{BitConverter.ToString(hashed)}|");
             return hashed;
+        }*/
+        
+        public byte[] ComputePasswordHash(string pass, byte[] salt, int iterations = 5000, int hashByteSize = 64)
+        {
+            using (Rfc2898DeriveBytes hashGenerator = new Rfc2898DeriveBytes(pass, salt))
+            {
+                hashGenerator.IterationCount = iterations;
+                return hashGenerator.GetBytes(hashByteSize);
+            }
         }
         
         private static byte[] Combine(byte[] first, byte[] second)
--- Crypto/Crypto/LoginSystem/Program.cs
+++ Crypto/Crypto/LoginSystem/Program.cs
@@ -25,6 +25,7 @@ namespace LoginSystem
 
         static void Main(string[] args)
         {
+            bool user_authenticated = false;
             Hash hash = new Hash();
             var cs = "Host=localhost;Username=postgres;Password=s$cret;Database=h4loginsystem";
             using (var con = new NpgsqlConnection(cs))
@@ -37,79 +38,86 @@ namespace LoginSystem
                     Console.WriteLine($"Connected to postgreSQL version: {version}");
                 }
 
-                Console.Write("Username: ");
-                string username = Console.ReadLine();
-
-                Console.Write("Password: ");
-                string password = Console.ReadLine();
-
-                switch (Menu("Select\n1. Create user\n2. Login", 2))
+                while (!user_authenticated)
                 {
-                    case 1:
-                        byte[] salt = hash.GenerateSalt();
-                        byte[] hashed = hash.ComputePasswordHash(Encoding.UTF8.GetBytes(password), salt);
-                        // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(salt)}\nPassword hash: {BitConverter.ToString(hashed)}\nLen: {hashed.Length}");
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
 
-                        using (var cmd = new NpgsqlCommand("CALL create_user(@username, @password, @salt)", con))
-                        {
-                            cmd.Parameters.AddWithValue("username", username);
-                            cmd.Parameters.AddWithValue("password", hashed);
-                            cmd.Parameters.AddWithValue("salt", salt);
-                            ;
-                            cmd.ExecuteNonQuery();
-                        }
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
 
-                        break;
-                    case 2:
-                        byte[] db_password = new byte[64];
-                        byte[] db_salt = new byte[16];
-                        int db_attempts = 0;
+                    switch (Menu("Select\n1. Create user\n2. Login", 2))
+                    {
+                        case 1:
+                            byte[] salt = hash.GenerateSalt();
+                            byte[] hashed = hash.ComputePasswordHash(password, salt);
+                            // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(salt)}\nPassword hash: {BitConverter.ToString(hashed)}\nLen: {hashed.Length}");
 
-                        using (var cmd = new NpgsqlCommand("get_user", con))
-                        {
-                            cmd.CommandType = CommandType.StoredProcedure;
-                            cmd.Parameters.AddWithValue("in_username", username);
-                            using (var reader = cmd.ExecuteReader())
+                            using (var cmd = new NpgsqlCommand("CALL create_user(@username, @password, @salt)", con))
                             {
-                                Console.WriteLine();
-                                while (reader.Read())
+                                cmd.Parameters.AddWithValue("username", username);
+                                cmd.Parameters.AddWithValue("password", hashed);
+                                cmd.Parameters.AddWithValue("salt", salt);
+                                ;
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            break;
+                        case 2:
+                            byte[] db_password = new byte[64];
+                            byte[] db_salt = new byte[16];
+                            int db_attempts = 0;
+
+                            using (var cmd = new NpgsqlCommand("get_user", con))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("in_username", username);
+                                using (var reader = cmd.ExecuteReader())
                                 {
-                                    reader.GetString(0);
-                                    reader.GetBytes(1, 0, db_password, 0, 64);
-                                    reader.GetBytes(2, 0, db_salt, 0, 16);
-                                    db_attempts = reader.GetInt16(3);
+                                    Console.WriteLine();
+                                    while (reader.Read())
+                                    {
+                                        reader.GetString(0);
+                                        reader.GetBytes(1, 0, db_password, 0, 64);
+                                        reader.GetBytes(2, 0, db_salt, 0, 16);
+                                        db_attempts = reader.GetInt16(3);
+                                    }
                                 }
                             }
-                        }
 
-                        if (db_attempts > 5)
-                        {
-                            Console.WriteLine("User account locked due to too many login attempts");
-                        }
-                        else
-                        {
-                            
-                            byte[] hashFromUser = hash.ComputePasswordHash(Encoding.UTF8.GetBytes(password), db_salt);
-                            // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(db_salt)}\nPassword hash from user: {BitConverter.ToString(hashFromUser)}\nPassword hash in DB: {BitConverter.ToString(db_password)}\nLen: {hashFromUser.Length}");
-                            if (hash.CheckAuthenticity(hashFromUser, db_password))
+                            if (db_attempts > 5)
                             {
-                                Console.WriteLine("Password correct!");
+                                Console.WriteLine("User account locked due to too many login attempts");
                             }
                             else
                             {
-                                Console.WriteLine("Password incorrect :(");
-                                using (var cmd = new NpgsqlCommand("CALL wrong_password(@username, @attempts)", con))
+
+                                byte[] hashFromUser =
+                                    hash.ComputePasswordHash(password, db_salt);
+                                // Console.WriteLine($"Password: |{password}|\nSalt:{BitConverter.ToString(db_salt)}\nPassword hash from user: {BitConverter.ToString(hashFromUser)}\nPassword hash in DB: {BitConverter.ToString(db_password)}\nLen: {hashFromUser.Length}");
+                                if (hash.CheckAuthenticity(hashFromUser, db_password))
                                 {
-                                    cmd.Parameters.AddWithValue("username", username);
-                                    cmd.Parameters.AddWithValue("attempts", db_attempts + 1);
-                                    ;
-                                    cmd.ExecuteNonQuery();
+                                    Console.WriteLine("Password correct!");
+                                    user_authenticated = true;
                                 }
+                                else
+                                {
+                                    Console.WriteLine("Password incorrect :(");
+                                    db_attempts++;
+                                    using (var cmd = new NpgsqlCommand("CALL wrong_password(@username, @attempts)",
+                                        con))
+                                    {
+                                        cmd.Parameters.AddWithValue("username", username);
+                                        cmd.Parameters.AddWithValue("attempts", db_attempts);
+                                        ;
+                                        cmd.ExecuteNonQuery();
+                                    }
+                                }
+
                             }
 
-                        }
-
-                        break;
+                            break;
+                    }
                 }
             }
         }
