3fe4b1c75826b0cb8c3290697a068e3685ba5f91
goket1 <goket@protonmail.com>
2020-11-19 11:00:01 +0100

    Created the tennis ball assignment, it works but the animation is buggy

--- Android/CurrencyConverter/app/src/main/java/com/example/currencyconverter/ApiConnector.java
+++ Android/CurrencyConverter/app/src/main/java/com/example/currencyconverter/ApiConnector.java
@@ -9,7 +9,9 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -27,21 +29,45 @@ public class ApiConnector {
 
     public ApiConnector(final Context context) {
         this.context = context;
-        volleyQueue = Volley.newRequestQueue(context);
+        RequestQueue requestQueue;
+
+    // Instantiate the cache
+            Cache cache = new DiskBasedCache(context.getCacheDir(), 1024 * 1024); // 1MB cap
+
+    // Set up the network to use HttpURLConnection as the HTTP client.
+            Network network = new BasicNetwork(new HurlStack());
+
+    // Instantiate the RequestQueue with the cache and network.
+            requestQueue = new RequestQueue(cache, network);
+
+    // Start the queue
+            requestQueue.start();
+
     }
 
     public JSONObject get() {
         System.out.println("Api connector get called");
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, baseUrl, null,
-                        response -> {
-                            System.out.println("Got response from API: " + response.toString());
 
-                        }, error -> {
-                    System.out.println("Got error from API");
+
+        String url ="http://www.example.com";
+
+        // Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("Got response from API: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("Got an error from the API: " + error.getMessage());
+                    }
                 });
+
         // Add the request to the RequestQueue.
-        this.volleyQueue.add(jsonObjectRequest);
+        volleyQueue.add(stringRequest);
         return null;
     }
 
---
+++ Android/TennisBolden/app/src/main/java/com/example/tennisbolden/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.tennisbolden;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    SensorManager sensorManager = null;
+    TextView sensorTextView = null;
+    ImageView tennisBallImageView = null;
+
+    List list;
+
+
+    //region Vars for translating the ball position
+    private int accelerationAmplification = 50;
+
+    private float maxX = 0;
+    private float maxY = 0;
+
+    private float posX = 0;
+    private float posY = 0;
+    //endregion
+
+    SensorEventListener sensorEventListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+            float[] values = event.values;
+            MoveTennisBall(-values[0], -values[1]);
+        }
+    };
+
+    // TODO Make it more generic?
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get a SensorManager instance
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Find UI elements
+        sensorTextView = (TextView) findViewById(R.id.sensorTextView);
+        tennisBallImageView = (ImageView) findViewById(R.id.tennisBallImageView);
+
+        // Find the accelerometer
+        list = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (list.size() > 0) {
+            // Accelerometer found
+            sensorManager.registerListener(sensorEventListener, (Sensor) list.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            // No Accelerometer found
+            Toast.makeText(getBaseContext(), "Error: No Accelerometer.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        // region Find max width and height of the application
+        // We do this in onWindowFocusChanged, because in onCreate the view has not et been initialised
+        // Find the constraint layout to get it's size
+        ConstraintLayout mainConstraint = (ConstraintLayout) findViewById(R.id.mainConstraint);
+
+        // Get the size of the mainConstraint and minus it with the size of the ball
+        maxY = mainConstraint.getHeight() - tennisBallImageView.getHeight();
+        maxX = mainConstraint.getWidth() - tennisBallImageView.getWidth();
+
+        // Set tennis ball position to the middle
+        posX = maxX / 2;
+        posY = maxY / 2;
+
+        // Print the dimensions
+        System.out.println("Found max\nX: " + maxX + "\nY: " + maxY);
+        //endregion
+    }
+
+    private void MoveTennisBall(float x, float y) {
+        // Set the last position for the animation
+        float lastPosX = posX;
+        float lastPosY = posY;
+
+        // Find the new position
+        posX += x * accelerationAmplification;
+        posY -= y * accelerationAmplification;
+
+        // Check that the position is not in the negatives, if it is, set it to zero so we don't exceed the screen size
+        if (posX < 0) {
+            posX = 0;
+        // Check if the position is over max
+        } else if (posX > maxX) {
+            posX = maxX;
+        }
+
+        if (posY < 0) {
+            posY = 0;
+        } else if (posY > maxY) {
+            posY = maxY;
+        }
+
+        // Set the position
+        // tennisBallImageView.setX(posX);
+        // tennisBallImageView.setY(posY);
+
+        TranslateAnimation animation = new TranslateAnimation(lastPosX, posX,
+                lastPosY, posY);          //  new TranslateAnimation(xFrom,xTo, yFrom,yTo)
+        animation.setDuration(5);         // animation duration
+        //animation.setFillAfter(true);
+
+        tennisBallImageView.startAnimation(animation);  // start animation
+
+        // Display the position to the user with the textview
+        sensorTextView.setText("x: " + posX + "\ny: " + posY);
+    }
+
+
+    @Override
+    protected void onStop() {
+        // Unregister the accelerometer if it's registered
+        if (list.size() > 0) {
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+        super.onStop();
+    }
+}
